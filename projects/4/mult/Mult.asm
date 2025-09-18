e36c354e8c38d98167addcadc586b4fcdd55e3f4
michelefdev <michele.franzoni.dev@gmail.com>
2025-09-18 08:54:20 +0200

    feat(project/4/mult): implemented multiplication

--- projects/4/mult/Mult.asm
+++ projects/4/mult/Mult.asm
@@ -7,4 +7,33 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 // The algorithm is based on repetitive addition.
 
-//// Replace this comment with your code.
+@R2
+M=0
+
+@i
+M=0
+
+(LOOP)
+@i
+D=M     // D = i
+@R1
+D=M-D 	// D = R1 - i
+
+@END  	// if R1 - i = 0 then R1=i then program ends
+D;JEQ
+
+@R0	    // otherwise add R0 to the total stored in R2 ...
+D=M
+
+@R2
+M=M+D
+
+@i
+M=M+1   // increment i
+
+@LOOP	// ... and jump back to the top of the loop
+0;JMP
+
+(END)   // End of program
+@END
+0;JMP
