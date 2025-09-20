e2a7091945f4b479a8e397f41c6c0bb10c984504
michelefdev <michele.franzoni.dev@gmail.com>
2025-09-20 10:34:43 +0200

    feat(projects/4/fill): addde draft for the fill program

--- projects/4/fill/Fill.asm
+++ projects/4/fill/Fill.asm
@@ -8,4 +8,65 @@
 // i.e. writes "black" in every pixel. When no key is pressed, 
 // the screen should be cleared.
 
-//// Replace this comment with your code.
+@KBD
+D=A
+@kbdaddress
+M=D
+
+    (LISTEN)
+    @SCREEN
+    D=A
+    @pointer 
+    M=D
+
+    @KBD
+    D=M
+
+    @WHITE
+    D;JEQ
+
+    @BLACK
+    0;JMP
+
+
+        (BLACK)
+        @pointer
+        A=M
+        M=-1
+
+        D=A
+        @kbdaddress
+        D=M-D
+
+        @LISTEN
+        D;JEQ
+
+        @pointer
+        M=M+1
+
+        @BLACK
+        0;JMP
+
+
+        (WHITE)
+        @pointer
+        A=M
+        M=-1
+
+        D=A
+        @kbdaddress
+        D=M-D
+
+        @LISTEN
+        D;JEQ
+
+        @pointer
+        M=M+1
+
+        @WHITE
+        0;JMP
+
+
+    (END)
+    @END
+    0;JMP
